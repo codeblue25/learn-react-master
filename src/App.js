7b263f714195dfd728e325c5ea0098bb6d400b7e
codeblue25 <chois1031@gmail.com>
2024-01-29 23:46:35 +0900

    Install styled-components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: #fff;
+`;
+
 function App() {
-  return null
+  return (
+    <Wrapper>
+      <BoxOne>
+        <Text>Hello !</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Wrapper>
+
+    // <div style={{display: "flex"}}>
+    //   <div style={{ backgroundColor: "teal", width: 100, height: 100 }}>
+    //      <span style={{ color: "#fff" }}>Hello !</span>
+    //   </div>
+    //   <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+    // </div>
+  );
 }
 
 export default App;
