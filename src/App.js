766d77cd5fc7f618fe1722bedfda2913c04b9afe
codeblue25 <chois1031@gmail.com>
2024-01-30 23:27:44 +0900

    Learn about pseudo selector part1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
 `;
+
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${rotateAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+
+    &:hover {
+      font-size: 48px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-// const Circle = styled.div`
-//   background-color: ${(props) => props.bgColor};
-//   width: 100px;
-//   height: 100px;
-//   border-radius: 50px;
-// `;
 
 function App() {
   return (
     <Wrapper>
-      <Box bgColor="teal" />
-      <Box bgColor="tomato" />
-      <Circle bgColor="skyblue" />
+      <Box>
+        <span>😇</span>
+      </Box>
     </Wrapper>
   );
 }
