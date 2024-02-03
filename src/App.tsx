e08ba5976e21167bab83b1a82a383eaa6062df56
codeblue25 <chois1031@gmail.com>
2024-02-03 17:21:35 +0900

    Typing the props with interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${(props) => props.theme.backgroundColor};
-`;
-
-const Title = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Title>Hello world !</Title>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </div>
   );
 }
 
