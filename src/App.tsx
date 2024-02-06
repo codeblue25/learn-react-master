f23d4360bed43cbd4c4436d222e5fcb02256556f
codeblue25 <chois1031@gmail.com>
2024-02-06 22:56:08 +0900

    How to make theme part2

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
-import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    // console.log(event.currentTarget.value);
-    const {
-      currentTarget: { value },
-    } = event;
-
-    setValue(value); // type: string
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello " + value + " !");
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>LOG IN</button>
-      </form>
-    </div>
+    <Container>
+      <Title>Hello</Title>
+    </Container>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const lightTheme: DefaultTheme = {
+  textColor: "#111",
+  backgroundColor: "whitesmoke",
+  buttonColor: "tomato",
+};
+
+export const darkTheme: DefaultTheme = {
+  textColor: "whitesmoke",
+  backgroundColor: "#111",
+  buttonColor: "teal",
+};
