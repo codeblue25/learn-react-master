bea562c34b09d2a7cc5a4ffc1cc83a77f5b1a3fe
codeblue25 <chois1031@gmail.com>
2024-02-06 22:33:13 +0900

    Typing the state and event

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+
+    setValue(value); // type: string
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello " + value + " !");
+  };
+
   return (
     <div>
-      <Circle bgColor="teal" borderColor="red" />
-      <Circle bgColor="tomato" text="I'm here" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>LOG IN</button>
+      </form>
     </div>
   );
 }
