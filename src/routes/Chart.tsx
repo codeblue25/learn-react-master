654def29cbcdbb131186904f5a584f2a50f31e06
codeblue25 <chois1031@gmail.com>
2024-02-24 23:40:03 +0900

    Visualize price data with ApexChart part1

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface IHistorical {
   time_open: number;
@@ -19,7 +20,40 @@ function Chart({ coinId }: ChartProps) {
   const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart .. "
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => Number(price.close)) as number[],
+            },
+          ]}
+          options={{
+            chart: {
+              width: 500,
+              height: 200,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            grid: { show: false },
+            theme: { mode: "dark" },
+            stroke: { curve: "smooth", width: 3 },
+            yaxis: { show: false },
+            xaxis: {
+              axisTicks: { show: false },
+              axisBorder: { show: false },
+              labels: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
