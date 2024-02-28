a31a82dfa227442ef933b1d03f0169de122d955d
codeblue25 <chois1031@gmail.com>
2024-02-28 18:23:44 +0900

    Setup for todo list project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import TodoList from "./TodoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+3:wght@300;400&display=swap')
@@ -68,6 +69,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <TodoList />
     </>
   );
 }
