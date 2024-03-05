b83ffc9fefa02dda129838e583225dc4501229d8
codeblue25 <chois1031@gmail.com>
2024-03-05 23:13:57 +0900

    Check category with anonymous function

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,7 +1,23 @@
 import { IToDo } from "../atoms";
 
-function Todo({ text }: IToDo) {
-  return <li>{text}</li>;
+function Todo({ text, category }: IToDo) {
+  const onClick = (selectedCategory: IToDo["category"]) => {
+    console.log(selectedCategory);
+  };
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>DOING</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>TO_DO</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>DONE</button>
+      )}
+    </li>
+  );
 }
 
 export default Todo;
