3a74e439492b392326cc1d1df4cefe3e7cde11b0
codeblue25 <chois1031@gmail.com>
2024-03-13 10:42:30 +0900

    Style Kanban board with droppable snapshot

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,11 +4,12 @@ import DragabbleCard from "./DragabbleCard";
 
 const Wrapper = styled.div`
   width: 300px;
-  padding: 20px 10px;
   padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h2`
@@ -16,6 +17,23 @@ const Title = styled.h2`
   font-weight: 600;
   margin-bottom: 10px;
   font-size: 18px;
+`;
+
+interface IAreaProps {
+  $isDraggingOver: boolean;
+  $isDraggingFromThis: boolean;
+}
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.$isDraggingOver
+      ? "#dfe6e9"
+      : props.$isDraggingFromThis
+      ? "#b2bec3"
+      : "transparent"};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
+  padding: 20px;
 `;
 
 interface IBoardProps {
@@ -28,13 +46,18 @@ function Board({ todoArr, boardId }: IBoardProps) {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(magic) => (
-          <div ref={magic.innerRef} {...magic.droppableProps}>
+        {(magic, snapshot) => (
+          <Area
+            $isDraggingOver={snapshot.isDraggingOver}
+            $isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+            ref={magic.innerRef}
+            {...magic.droppableProps}
+          >
             {todoArr.map((todo, index) => (
               <DragabbleCard key={todo} todo={todo} index={index} />
             ))}
             {magic.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
--- src/components/DragabbleCard.tsx
+++ src/components/DragabbleCard.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ $isDragging: boolean }>`
   border-radius: 5px;
   margin-bottom: 5px;
   padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
+  background-color: ${(props) =>
+    props.$isDragging ? "#e4f2ff" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.$isDragging ? "0px 2px 5px rgba(0,0,0,0.5)" : "none"};
 `;
 
 interface IDragabbleCardProps {
@@ -15,11 +18,12 @@ interface IDragabbleCardProps {
 }
 
 function DragabbleCard({ todo, index }: IDragabbleCardProps) {
-  console.log(todo, "has been rendered");
+  // console.log(todo, "has been rendered");
   return (
     <Draggable draggableId={todo} index={index} key={todo}>
-      {(magic) => (
+      {(magic, snapshot) => (
         <Card
+          $isDragging={snapshot.isDragging}
           ref={magic.innerRef}
           {...magic.dragHandleProps}
           {...magic.draggableProps}
