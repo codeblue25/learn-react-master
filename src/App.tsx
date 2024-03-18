5d8a154947f303e33d1242b6041147ee9774767e
codeblue25 <chois1031@gmail.com>
2024-03-18 15:26:00 +0900

    Create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Routes/Home";
 import Tvshow from "./Routes/Tvshow";
 import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route path="/">
           <Home />
