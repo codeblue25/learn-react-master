f946c728022a7c9f25822698c4566ea79dbb1f55
codeblue25 <chois1031@gmail.com>
2024-03-21 16:06:34 +0900

    Implement movie info box

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -58,12 +58,65 @@ const Box = styled(motion.div)<{ bgPhoto: string }>`
   color: #000;
   background-image: url(${(props) => props.bgPhoto});
   background-size: cover;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`;
+
+const Info = styled(motion.div)`
+  position: absolute;
+  bottom: -82px;
+  padding: 16px;
+  width: 100%;
+  height: 82px;
+  opacity: 0;
+  color: ${(props) => props.theme.white.lighter};
+  background-color: ${(props) => props.theme.black.darker};
+  h4 {
+    font-weight: 700;
+    font-size: 18px;
+  }
+  span {
+    font-size: 16px;
+  }
+`;
+
+const RankSvg = styled.svg`
+  margin-right: 4px;
+  width: 15px;
+  height: 15px;
+  color: red;
+`;
+const MoreSvg = styled.svg`
+  position: absolute;
+  left: 88%;
+  width: 25px;
+  height: 25px;
 `;
 
 const rowVariants = {
   hidden: { x: window.innerWidth + 10 },
   visible: { x: 0 },
   exit: { x: -window.innerWidth - 10 },
+};
+
+const boxVariants = {
+  normal: { scale: 1 },
+  hover: {
+    scale: 1.2,
+    y: -50,
+    transition: { delay: 0.5, duration: 0.3, type: "tween" },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: { delay: 0.5, duration: 0.3, type: "tween" },
+  },
 };
 
 function Home() {
@@ -116,7 +169,35 @@ function Home() {
                     <Box
                       key={movie.id}
                       bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                    />
+                      variants={boxVariants}
+                      initial="normal"
+                      whileHover="hover"
+                      transition={{ type: "tween" }}
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                        <RankSvg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 576 512"
+                        >
+                          <path
+                            fill="#ff0000"
+                            d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+                          />
+                        </RankSvg>
+
+                        <span>{movie.vote_average}</span>
+                        <MoreSvg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 512 512"
+                        >
+                          <path
+                            fill="#ffffff"
+                            d="M256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM135 241c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l87 87 87-87c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9L273 345c-9.4 9.4-24.6 9.4-33.9 0L135 241z"
+                          />
+                        </MoreSvg>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
