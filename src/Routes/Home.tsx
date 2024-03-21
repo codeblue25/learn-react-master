81a4e519c2009d6e99ba2937dff7a2683affb882
codeblue25 <chois1031@gmail.com>
2024-03-21 10:57:58 +0900

    Implement slider with AnimatePresence

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "react-query";
 import { IMovieResult, getMovies } from "../api";
 import styled from "styled-components";
 import { makeImagePath } from "../utils";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   background-color: #000;
@@ -35,11 +37,48 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  position: absolute;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  height: 200px;
+  font-size: 24px;
+  color: #000;
+  background-color: #fff;
+`;
+
+const rowVariants = {
+  hidden: { x: window.innerWidth + 10 },
+  visible: { x: 0 },
+  exit: { x: -window.innerWidth - 10 },
+};
+
 function Home() {
   const { data, isLoading } = useQuery<IMovieResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => {
+    if (leaving) return;
+    setLeaving(true);
+    setIndex((prev) => prev + 1);
+  };
+  const [leaving, setLeaving] = useState(false);
+  const toggleLeaving = () => {
+    setLeaving((prev) => !prev);
+  };
 
   return (
     <Wrapper>
@@ -47,10 +86,30 @@ function Home() {
         <Loader>Loading ..</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+          <Banner
+            bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+            onClick={increaseIndex}
+          >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 2 }}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
